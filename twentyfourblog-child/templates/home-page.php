8230ec14f7752694ccaf7cb3705f9746a4ad2b51
tuannt <tuannt@shopdunk.vn>
2020-03-27 07:46:30 +0700

    Update 2 buton home

--- twentyfourblog-child/templates/home-page.php
+++ twentyfourblog-child/templates/home-page.php
@@ -1,4 +1,8 @@
 <div class="home-mobile-container">
+	<div class="home-cate">
+		<div class="home-km"><a href="https://blog.karseell.com/chuyen-muc/khuyen-mai/">Khuyến mại hot</a></div>
+		<div class="home-sk"><a href="https://blog.karseell.com/chuyen-muc/su-kien/">Sự kiện nổi bật</a></div>
+	</div>
 	<?php echo new_home('','5','1'); ?>
 	<?php echo new_home('','15',''); ?>
 </div>
